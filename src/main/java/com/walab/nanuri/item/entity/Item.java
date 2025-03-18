1ef3bcc78cb224b9d04963c52ad4b48e9a12236d
AnJunYong02 <happihanjy@naver.com>
2025-03-18 15:07:50 +0900

    feat: item 초기 구축

---
+++ src/main/java/com/walab/nanuri/item/entity/Item.java
@@ -0,0 +1,12 @@
+package com.walab.nanuri.item.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Item {
+
+}
