cc8a35eb9ba262b54fc933b4e75279ac73818aa3
AnJunYong02 <happihanjy@naver.com>
2025-03-18 20:36:28 +0900

    feat: itemEntity 생성

---
+++ src/main/java/com/walab/nanuri/item/controller/ItemController.java
@@ -0,0 +1,10 @@
+package com.walab.nanuri.item.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/item")
+public class ItemController {
+
+}
--- src/main/java/com/walab/nanuri/item/entity/Item.java
+++ src/main/java/com/walab/nanuri/item/entity/Item.java
@@ -1,12 +1,43 @@
 package com.walab.nanuri.item.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class Item {
+    @Id
+    @Column(name = "id", nullable=false, length=50)
+    private long id;
 
+    @Column(nullable = false, length=100)
+    private String title;
+
+    @Column(length = 255)
+    private String description;
+
+    @Column(length = 50)
+    private String place;
+
+    @Column(name="view_count")
+    private Integer viewCount;
+
+    @Column(length = 50)
+    private String category;
+
+    @Column(name="user_id", nullable=false)
+    private long userId;
+
+    @Column(name="is_finished")
+    private Boolean isFinished;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
