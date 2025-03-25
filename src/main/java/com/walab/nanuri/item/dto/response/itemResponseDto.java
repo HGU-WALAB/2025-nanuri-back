bc29050979dce719495a6c8ca7b7eb5954d5eb39
AnJunYong02 <happihanjy@naver.com>
2025-03-25 14:08:52 +0900

    Feat: createResponseDto

---
+++ src/main/java/com/walab/nanuri/item/dto/response/itemResponseDto.java
@@ -0,0 +1,37 @@
+package com.walab.nanuri.item.dto.response;
+
+import com.walab.nanuri.item.entity.Item;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+public class itemResponseDto {
+    private Long id;
+    private String title;
+    private String description;
+//    private String place;
+    private Integer viewCount;
+    private String category;
+    private Boolean isFinished;
+    private LocalDateTime createdTime;
+    private Integer wishCount;
+    private Boolean isOwner;
+
+//    public Item toEntity(){
+//        return Item.builder()
+//                .id(this.id)
+//                .title(this.title)
+//                .description(this.description)
+//                .viewCount(this.viewCount)
+//                .category(this.category)
+//                .isFinished(this.isFinished)
+//                .createdTime(this.createdTime)
+//                .build();
+//    }
+
+}
