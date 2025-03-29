4ab5d5adcaa4eadb239ae087801978e678977c65
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-03-29 18:28:35 +0900

    chore: reorganize package structure for History

--- src/main/java/com/walab/nanuri/history/dto/response/ApplicantDto.java
+++ src/main/java/com/walab/nanuri/history/dto/response/ApplicantDto.java
@@ -1,4 +1,4 @@
-package com.walab.nanuri.history.dto.request;
+package com.walab.nanuri.history.dto.response;
 
 import com.walab.nanuri.commons.util.Time;
 import com.walab.nanuri.history.entity.History;
@@ -17,11 +17,13 @@ import java.sql.Timestamp;
 //(물건 나눔자만 볼 수 있음)
 // 나눔 신청한 사람 리스트
 public class ApplicantDto {
+    private Long historyId;
     private String nickName;
     private String applicationTime;
 
     public static ApplicantDto from(History history, User user){
         return ApplicantDto.builder()
+                .historyId(history.getId())
                 .nickName(user.getNickname())
                 .applicationTime(Time.calculateTime(Timestamp.valueOf(history.getCreatedTime())))
                 .build();
