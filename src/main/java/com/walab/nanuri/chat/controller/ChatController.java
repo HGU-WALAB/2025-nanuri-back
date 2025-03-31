ab6b8b0f7d277c55f9d1391860a2465a6b3366ac
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-03-31 23:17:47 +0900

    feat: add REST API endpoints for chat rooms and messages

---
+++ src/main/java/com/walab/nanuri/chat/controller/ChatController.java
@@ -0,0 +1,45 @@
+package com.walab.nanuri.chat.controller;
+
+import com.walab.nanuri.chat.dto.request.ChatRoomRequestDto;
+import com.walab.nanuri.chat.dto.response.ChatMessageResponseDto;
+import com.walab.nanuri.chat.dto.response.ChatRoomResponseDto;
+import com.walab.nanuri.chat.service.ChatMessageService;
+import com.walab.nanuri.chat.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/chat")
+public class ChatController {
+    private final ChatRoomService chatRoomService;
+    private final ChatMessageService chatMessageService;
+
+    @GetMapping("/room")
+    public ResponseEntity<List<ChatRoomResponseDto>> getRooms(@AuthenticationPrincipal String uniqueId){
+        return ResponseEntity.ok().body(chatRoomService.getChatRooms(uniqueId));
+    }
+
+    @GetMapping("/room/{roomId}/messages")
+    public ResponseEntity<List<ChatMessageResponseDto>> getMessagesInRoom(
+                                                            @AuthenticationPrincipal String uniqueId,
+                                                            @PathVariable Long roomId,
+                                                            @RequestBody ChatRoomRequestDto.ChatRoomUserValidationRequest request,
+                                                            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime cursor
+    ){
+        return ResponseEntity.ok().body(chatMessageService.getChatMessages(uniqueId , roomId ,request, cursor));
+    }
+
+    @DeleteMapping("/room/{roomId}")
+    public ResponseEntity<?> deleteRoom(@AuthenticationPrincipal String uniqueId, @PathVariable Long roomId) {
+        chatRoomService.deleteChatRoom(uniqueId, roomId);
+        return ResponseEntity.ok().build();
+    }
+}
