158040e10044f32530bae6a46b762bce387ce169
JunYong An <happihanjy@naver.com>
2025-04-03 20:02:22 +0900

    feat: add signUp attribute

--- src/main/java/com/walab/nanuri/auth/dto/request/SignupRequestDto.java
+++ src/main/java/com/walab/nanuri/auth/dto/request/SignupRequestDto.java
@@ -2,8 +2,14 @@ package com.walab.nanuri.auth.dto.request;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class SignupRequestDto {
     private String uniqueId;
     private String nickname;
+    private String mbti;
+    private List<String> interestTag;
+    private String hobby;
+    private String introduction;
 }
