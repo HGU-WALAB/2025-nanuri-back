9104d2ab1894b4d5cfc3b9ebccbfbd30572a3acd
JunYong An <happihanjy@naver.com>
2025-04-05 23:55:14 +0900

    feat: check nickname duplicate

--- src/main/java/com/walab/nanuri/user/repository/UserRepository.java
+++ src/main/java/com/walab/nanuri/user/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.walab.nanuri.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByNickname(String nickname);
 }
