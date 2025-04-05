ac621d076911a15bc78b178d3862d4b27b7e65d3
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-04-06 02:46:41 +0900

    chore: remove unused method(s)

--- src/main/java/com/walab/nanuri/wish/repository/WishRepository.java
+++ src/main/java/com/walab/nanuri/wish/repository/WishRepository.java
@@ -4,10 +4,7 @@ import com.walab.nanuri.wish.entity.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface WishRepository extends JpaRepository<Wish, Long> {
-    Optional<Wish> findWishByUniqueIdAndItemId(String uniqueId, Long itemId);
-    Optional<Wish> findWishByUniqueIdAndId(String uniqueId, Long id);
     List<Wish> findAllByUniqueId(String uniqueId);
 }
