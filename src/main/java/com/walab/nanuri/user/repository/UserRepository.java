b310ddaea3d95cb41eddefc17470f7c35fee4c75
JunYong An <happihanjy@naver.com>
2025-04-06 02:19:05 +0900

    feat: add 'getOtherUserInfo'

--- src/main/java/com/walab/nanuri/user/repository/UserRepository.java
+++ src/main/java/com/walab/nanuri/user/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.walab.nanuri.user.repository;
 
+import com.walab.nanuri.user.dto.UserDto;
+import com.walab.nanuri.user.dto.UserResponseDto;
 import com.walab.nanuri.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByNickname(String nickname);
 }
