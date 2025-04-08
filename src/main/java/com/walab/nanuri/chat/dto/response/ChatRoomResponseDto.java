20f1e37ead66e4444be217f47c29ed6251d5c759
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-04-08 22:43:44 +0900

    feat: Add dynamic post title and postId to ChatRoomResponseDto

--- src/main/java/com/walab/nanuri/chat/dto/response/ChatRoomResponseDto.java
+++ src/main/java/com/walab/nanuri/chat/dto/response/ChatRoomResponseDto.java
@@ -3,6 +3,7 @@ package com.walab.nanuri.chat.dto.response;
 import com.walab.nanuri.chat.entity.ChatRoom;
 import com.walab.nanuri.item.entity.Item;
 import com.walab.nanuri.user.entity.User;
+import com.walab.nanuri.want.entity.WantPost;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,22 +12,24 @@ import lombok.Getter;
 public class ChatRoomResponseDto {
     private Long roomId;
     private Long itemId;
-    private String itemTitle;
+    private Long postId;
+    private String title;
     private String itemImage;
     private String opponentId;
     private String opponentName;
     private boolean isSeller;
 
-    public static ChatRoomResponseDto from(ChatRoom room, String myId, User opponent, Item item, String ItemUrl) {
+    public static ChatRoomResponseDto from(ChatRoom room, String myId, User opponent, Item item, String ItemUrl, WantPost post) {
         boolean isSeller = room.getSellerId().equals(myId);
         String opponentId = isSeller ? room.getReceiverId() : room.getSellerId();
 
         return ChatRoomResponseDto.builder()
                 .roomId(room.getId())
                 .itemId(room.getItemId())
+                .postId(room.getPostId())
                 .opponentId(opponentId)
                 .opponentName(opponent.getName())
-                .itemTitle(item.getTitle())
+                .title(item == null ? post.getTitle() : item.getTitle())
                 .itemImage(ItemUrl)
                 .isSeller(isSeller)
                 .build();
