122c441730c9e0771098a85eaa0355cf2fdadeb4
Sumin Jeon <wjstnals1211@gmail.com>
2025-04-21 00:26:43 +0900

    refactor: rename wishId to itemId in deleteWish method and update repository query

--- src/main/java/com/walab/nanuri/wish/controller/WishController.java
+++ src/main/java/com/walab/nanuri/wish/controller/WishController.java
@@ -28,9 +28,9 @@ public class WishController {
     }
 
     //관심 목록 삭제
-    @DeleteMapping("/{wishId}")
-    public ResponseEntity<Void> deleteWish(@AuthenticationPrincipal String uniqueId, @PathVariable Long wishId) {
-        wishService.deleteWish(uniqueId, wishId);
+    @DeleteMapping("/{itemId}")
+    public ResponseEntity<Void> deleteWish(@AuthenticationPrincipal String uniqueId, @PathVariable Long itemId) {
+        wishService.deleteWish(uniqueId, itemId);
         return ResponseEntity.ok().body(null);
     }
 
