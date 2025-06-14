8113be52c76056ca29485500e9375ba725184c3d
JunYong An <happihanjy@naver.com>
2025-06-15 00:35:50 +0900

    feat: Add wishCount, viewCount from ReceivedItemDto

--- src/main/java/com/walab/nanuri/history/dto/response/ReceivedItemDto.java
+++ src/main/java/com/walab/nanuri/history/dto/response/ReceivedItemDto.java
@@ -1,5 +1,6 @@
 package com.walab.nanuri.history.dto.response;
 
+import com.walab.nanuri.commons.util.ShareStatus;
 import com.walab.nanuri.commons.util.Time;
 import com.walab.nanuri.item.entity.Item;
 import lombok.AllArgsConstructor;
@@ -22,12 +23,16 @@ public class ReceivedItemDto {
     private String description;
     private String category;
     private String image;
+    private ShareStatus shareStatus;
+    private Boolean wishStatus;
     private String createdTime;
     private String modifiedTime;
+    private Integer wishCount;
+    private Integer chatCount;
+    private Integer viewCount;
+    private LocalDateTime deadline;
 
-
-
-    public static ReceivedItemDto from(Item item, Long historyId, String image){
+    public static ReceivedItemDto from(Item item, Long historyId, String image, Boolean wishStatus) {
         return ReceivedItemDto.builder()
                 .historyId(historyId)
                 .itemId(item.getId())
@@ -35,8 +40,14 @@ public class ReceivedItemDto {
                 .description(item.getDescription())
                 .category(item.getCategory().getKoreanName())
                 .image(image)
+                .shareStatus(item.getShareStatus())
+                .wishStatus(wishStatus)
                 .createdTime(item.getCreatedTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                 .modifiedTime(item.getModifiedTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .wishCount(item.getWishCount())
+                .chatCount(item.getChatCount())
+                .viewCount(item.getViewCount())
+                .deadline(item.getDeadline())
                 .build();
     }
 }
