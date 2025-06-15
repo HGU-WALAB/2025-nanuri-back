2a0650aeba6b59d0c4af33e73a53b33d5f0d16ea
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-06-15 20:53:08 +0900

    refactor: user 가 null 일 경우를 쳌

--- src/main/java/com/walab/nanuri/chat/dto/response/ChatMessageResponseDto.java
+++ src/main/java/com/walab/nanuri/chat/dto/response/ChatMessageResponseDto.java
@@ -16,14 +16,16 @@ public class ChatMessageResponseDto {
     private String senderNickname;
     private String receiverNickname;
     private LocalDateTime createdAt;
+    private ChatMessage.MessageType type;
     private String message;
 
     public static ChatMessageResponseDto from(ChatMessage chatMessage, User sender, User receiver) {
         return ChatMessageResponseDto.builder()
                 .roomId(chatMessage.getRoomId())
-                .senderNickname(sender.getNickname())
-                .receiverNickname(receiver.getNickname())
+                .senderNickname(sender != null ? sender.getNickname() : "null")
+                .receiverNickname(receiver != null ? receiver.getNickname() : "null")
                 .createdAt(chatMessage.getTimestamp())
+                .type(chatMessage.getType())
                 .message(chatMessage.getMessage())
                 .build();
     }
