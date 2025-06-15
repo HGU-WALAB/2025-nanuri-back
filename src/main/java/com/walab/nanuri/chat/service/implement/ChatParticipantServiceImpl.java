41e29759247e3d5e65d2195565d066d18a74e585
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-06-15 20:55:20 +0900

    feat: add the getChatParticipant  method in Chat Message implement

--- src/main/java/com/walab/nanuri/chat/service/implement/ChatParticipantServiceImpl.java
+++ src/main/java/com/walab/nanuri/chat/service/implement/ChatParticipantServiceImpl.java
@@ -1,8 +1,10 @@
 package com.walab.nanuri.chat.service.implement;
 
+import com.walab.nanuri.chat.entity.ChatParticipant;
 import com.walab.nanuri.chat.entity.ChatRoom;
 
 public interface ChatParticipantServiceImpl {
     void enterRoom(ChatRoom chatRoom, String userId);
     void leaveRoom(Long roomKey, String userId);
+    ChatParticipant getChatParticipant(String roomKey , String userId);
 }
